a079ad43981ace4f2e98c30170e43f5ea4aafff2
RobinLB99 <RJLB991102@hotmail.com>
2023-03-11 23:42:58 -0500

    Switch para cambio de tema

--- theme.js
+++ theme.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const sol = document.getElementById("sol")
   const luna = document.getElementById('luna')
 
+  let click = 1
+
   theme.addEventListener('click', () => {
 
     click += 1;
@@ -13,9 +15,9 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     if (click === 1) {
-      auto.setAttribute("style", "display: block")
-      sol.setAttribute("style", "display: none")
-      luna.setAttribute("style", "display: none")
+      auto.style.display = 'block'
+      sol.style.display = 'none'
+      luna.style.display = 'none'
 
       if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
 				// darkTheme()
@@ -24,30 +26,30 @@ document.addEventListener('DOMContentLoaded', () => {
 			}
 
     } else if (click === 2) {
-      auto.setAttribute("style", "display: none")
-      sol.setAttribute("style", "display: block")
-      luna.setAttribute("style", "display: none")
+      auto.style.display = 'none'
+      sol.style.display = 'block'
+      luna.style.display = 'none'
     } else {
-      auto.setAttribute("style", "display: none")
-      sol.setAttribute("style", "display: none")
-      luna.setAttribute("style", "display: block")
+      auto.style.display = 'none'
+      sol.style.display = 'none'
+      luna.style.display = 'block'
     }
   })
 
-  const readTheme = (theme) => {
-		if (theme === 2) {
-			lightTheme()
-		} else if (theme === 3) {
-			darkTheme()
-		} else if (theme === 1) {
-			if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-				darkTheme()
-			} else {
-				lightTheme()
-			}
-		}
-	}
+  // const readTheme = (theme) => {
+	// 	if (theme === 2) {
+	// 		lightTheme()
+	// 	} else if (theme === 3) {
+	// 		darkTheme()
+	// 	} else if (theme === 1) {
+	// 		if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+	// 			darkTheme()
+	// 		} else {
+	// 			lightTheme()
+	// 		}
+	// 	}
+	// }
 
-	readTheme(click);
+	// readTheme(click);
 
 })
